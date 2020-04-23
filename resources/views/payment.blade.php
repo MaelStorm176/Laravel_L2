8198094a0c30207d7dab392ff724c34a3697574e
Maelstorm <mael.jamin@gmail.com>
2020-04-23 10:45:52 +0200

    Mael 10:45 23/04/2020

--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -1,159 +1,214 @@
-<!DOCTYPE html>
-<html>
+@extends('layouts.app')
 <head>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.payment/1.0.1/jquery.payment.min.js"></script>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
-    <script src="https://kit.fontawesome.com/5b793409e3.js" crossorigin="anonymous"></script>
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-    <!-- Styles -->
-    <style type="text/css">
-        .panel-title {
-            display: inline;
-            font-weight: bold;
-        }
-        .display-table {
-            display: table;
-        }
-        .display-tr {
-            display: table-row;
-        }
-        .display-td {
-            display: table-cell;
-            vertical-align: middle;
-            width: 61%;
-        }
-    </style>
+    <!-- Material Design Bootstrap -->
+     <link href="/css/mdb.min.css" rel="stylesheet">
 </head>
-<body>
-<div class="container">
+@section('content')
+    <!--Main layout-->
+    <main class="mt-5 pt-4">
+        <div class="container wow fadeIn">
+            <!-- Heading -->
+            <h2 class="my-5 h2 text-center">Paiement</h2>
+            <!--Grid row-->
+            <div class="row">
 
-    <div class="row">
-        <div class="col-md-6 col-md-offset-3">
-            <div class="panel panel-default credit-card-box">
-                <div class="panel-heading display-table" >
-                    <div class="row display-tr" >
-                        <h3 class="panel-title display-td" >Payment Details</h3>
-                        <div class="display-td" >
-                            <img class="img-responsive pull-right" src="http://i76.imgup.net/accepted_c22e0.png">
-                        </div>
+                <!--Grid column-->
+                <div class="col-md-8 mb-4">
+
+                    <!--Card-->
+                    <div class="card">
+
+                        <!--Card content-->
+                        <form class="card-body"
+                            role="form"
+                            action="{{ route('stripe.post') }}"
+                            method="post"
+                            data-cc-on-file="false"
+                            data-stripe-publishable-key="{{ env('STRIPE_KEY') }}"
+                            id="payment-form">
+                            @csrf
+                            <?php
+                            $user =Auth::user()->id;
+                            $email =Auth::user()->email;
+                            ?>
+                            <input value="<?php echo $email;?>" name="user_email" type="hidden">
+                            <input value="<?php echo $user;?>" name="user_id" type="hidden">
+                            <input value="{{$prix_total}}" name="prix_total" type="hidden">
+                            <input value="{{$products}}" name="products" type="hidden">
+                            <input value="com_{{auth::user()->id}}_{{$prix_total}}" name="num_commande" type="hidden">
+                            <!--Grid row-->
+                            <div class="row">
+
+                                <!--Grid column-->
+                                <div class="col-md-6 mb-2">
+
+                                    <!--firstName-->
+                                    <div class="md-form ">
+                                        <input type="text" id="firstName" class="form-control" placeholder="Prénom" value="{{auth::user()->first_name}}">
+                                    </div>
+
+                                </div>
+                                <!--Grid column-->
+
+                                <!--Grid column-->
+                                <div class="col-md-6 mb-2">
+
+                                    <!--lastName-->
+                                    <div class="md-form">
+                                        <input type="text" id="lastName" class="form-control" placeholder="Nom de famille" value="{{auth::user()->last_name}}">
+                                    </div>
+
+                                </div>
+                                <!--Grid column-->
+
+                            </div>
+                            <!--Grid row-->
+
+                            <!--address-->
+                            <div class="md-form mb-5">
+                                <input type="text" id="address" placeholder="Adresse" class="form-control">
+                            </div>
+
+                            <!-- BOUTONS RADIO PAYPAL / CC  -->
+                            <div class="d-block my-3">
+                                <div class="custom-control custom-radio">
+                                    <input id="credit" name="paymentMethod" type="radio" class="custom-control-input" checked required>
+                                    <label class="custom-control-label" for="credit">Carte de crédit</label>
+                                </div>
+                                <div class="custom-control custom-radio">
+                                    <input id="paypal" name="paymentMethod" type="radio" class="custom-control-input" required>
+                                    <label class="custom-control-label" for="paypal">Paypal</label>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <!-- NOM CARTE CREDIT -->
+                                <div class="col-md-6 mb-3">
+                                    <label for="cc-name">Nom sur votre carte</label>
+                                    <input type="text" class="form-control" id="cc-name" placeholder="Ex : JEAN DUPONT" required>
+                                    <small class="text-muted">Veuillez écrire le nom présent sur votre carte en majuscule</small>
+                                    <div class="invalid-feedback">
+                                        Le nom est requis
+                                    </div>
+                                </div>
+                                <!-- N° CARTE CREDIT -->
+                                <div class="col-md-6 mb-3">
+                                    <label for="cc-number">N° Carte de crédit</label>
+                                    <input type="text" class="form-control" id="cc-number" name="card_number" placeholder="Ex : 5678 2345 7418 5693" required>
+                                    <div class="invalid-feedback">
+                                        Le N° de carte est requis
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <!-- DATE EXPIRATION -->
+                                <div class="col-md-3 mb-3">
+                                    <label for="cc-expiration">Expiration</label>
+                                    <input type="text" class="form-control" id="cc-expiration" name="card_mm_yyyy" placeholder="MM/YYYY" required>
+                                    <div class="invalid-feedback">
+                                        La date d'expiration est requise
+                                    </div>
+                                </div>
+                                <!-- CVV -->
+                                <div class="col-md-3 mb-3">
+                                    <label for="cc-expiration">CVV</label>
+                                    <input type="text" class="form-control" id="card_cvc" name="card_cvc" placeholder="Ex : 123" required>
+                                    <div class="invalid-feedback">
+                                        Le code de sécurité est requis
+                                    </div>
+                                </div>
+                            </div>
+                            <hr class="mb-4">
+                            <!-- BOUTON DE VALIDATION -->
+                            <button class="btn btn-primary btn-lg btn-block" type="submit">Valider le paiement</button>
+
+                        </form>
+
                     </div>
+                    <!--/.Card-->
+
                 </div>
-                <div class="panel-body">
+                <!--Grid column-->
 
-                    @if (Session::has('success'))
-                        <div class="alert alert-success text-center">
-                            <a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>
-                            <p>{{ Session::get('success') }}</p>
-                        </div>
-                    @endif
+                <!--Grid column-->
+                <div class="col-md-4 mb-4">
 
-                    <form
-                        role="form"
-                        action="{{ route('stripe.post') }}"
-                        method="post"
-                        class="require-validation"
-                        data-cc-on-file="false"
-                        data-stripe-publishable-key="{{ env('STRIPE_KEY') }}"
-                        id="payment-form">
-                        @csrf
-                        <?php
-                        $user =Auth::user()->id;
-                        $name =Auth::user()->username;
-                        ?>
-                        <div class='form-row row'>
-                            <div class='col-xs-12 form-group required'>
-                                <label class='control-label'>Pizza : Carbonara</label>
-                                <input value="Carbonara" name="nom_p" type="hidden">
-                                <label class='control-label'>Prix : 12 €</label>
-                                <input value="12" name="prix_p" type="hidden">
+                    <!-- Heading -->
+                    <h4 class="d-flex justify-content-between align-items-center mb-3">
+                        <span class="text-muted">Votre panier</span>
+                        <span class="badge badge-secondary badge-pill">{{$q_tot}} articles</span>
+                    </h4>
 
-                                <input value="<?php echo $name;?>" name="user_name" type="hidden">
-                                <input value="<?php echo $user;?>" name="user_id" type="hidden">
+                    <!-- Panier -->
+                    <ul class="list-group mb-3 z-depth-1">
+                        @foreach($products as $key)
+                            <li class="list-group-item d-flex justify-content-between lh-condensed">
+                                <div>
+                                    <h6 class="my-0">{{$key->nom}} x {{$key->quantite}}</h6>
+                                    <small class="text-muted">{{$key->statut}}</small>
+                                </div>
+                                <span class="text-muted">{{$key->promo}} €</span>
+                            </li>
+                        @endforeach
+                        <li class="list-group-item d-flex justify-content-between bg-light">
+                            <div class="text-success">
+                                <h6 class="my-0">Promo code</h6>
+                                <small>EXAMPLECODE</small>
+                            </div>
+                            <span class="text-success">-$5</span>
+                        </li>
+                        <li class="list-group-item d-flex justify-content-between lh-condensed">
+                            <div>
+                                <h6 class="my-0">TOTAL</h6>
+                                <small class="text-muted">TTC</small>
+                            </div>
+                            <span class="text-muted"><strong> {{$prix_total}} € </strong></span>
+                        </li>
+                    </ul>
+                    <!-- Panier -->
 
+                    <!-- Promo code -->
+                    <form class="card p-2">
+                        <div class="input-group">
+                            <input type="text" class="form-control" placeholder="Code promo" aria-label="Recipient's username" aria-describedby="basic-addon2">
+                            <div class="input-group-append">
+                                <button class="btn btn-secondary btn-md waves-effect m-0" type="button">APPLIQUER</button>
                             </div>
                         </div>
-
-                        <div class='form-row row'>
-                            <div class='col-xs-12 form-group required'>
-
-                                <label class='control-label'>Name on Card</label> <input
-                                    name="card_name" class='form-control' size='4' type='text'>
-                            </div>
-                        </div>
-
-                        <div class='form-row row'>
-                            <div class='col-xs-12 form-group card required'>
-                                <label class='control-label'>Card Number</label> <input
-                                     name="card_number" autocomplete='off' class='cc-number form-control card-number' size='20'
-                                    type='text' maxlength="16" value="4242424242424242">
-
-                            </div>
-                        </div>
-
-                        <div class='form-row row'>
-                            <div class='col-xs-12 col-md-4 form-group cvc required'>
-                                <label class='control-label'>CVC</label> <input autocomplete='off'
-                                                                                class='form-control card-cvc' placeholder='ex. 311' size='4'
-                                                                                name="card_cvc" type='text' value="123">
-                            </div>
-                            <div class='col-xs-12 col-md-4 form-group expiration required'>
-                                <label class='control-label'>Expiration Month</label> <input
-                                    name="card_mm" class='form-control card-expiry-month' placeholder='MM' size='2'
-                                    type='text' value="12">
-                            </div>
-                            <div class='col-xs-12 col-md-4 form-group expiration required'>
-                                <label class='control-label'>Expiration Year</label> <input
-                                    name="card_yyyy" class='form-control card-expiry-year' placeholder='YYYY' size='4'
-                                    type='text' value="2021">
-                            </div>
-                        </div>
-
-                        <div class='form-row row'>
-                            <div class='col-md-12 error form-group hide'>
-                                <div class='alert-danger alert'>Veuillez entrer un nom</div>
-
-                            </div>
-                        </div>
-
-                        <div class="row">
-                            <div class="col-xs-12">
-                                <button class="btn btn-primary btn-lg btn-block" type="submit">Pay Now ($100)</button>
-                            </div>
-                        </div>
-
                     </form>
-
+                    <!-- Promo code -->
 
                 </div>
+                <!--Grid column-->
+
             </div>
+            <!--Grid row-->
+
         </div>
-    </div>
+    </main>
+    <!--Main layout-->
+@endsection
 
-</div>
+@section('script')
+<!-- Initializations -->
+<!-- MDB core JavaScript -->
+<script type="text/javascript" src="js/mdb.min.js"></script>
+<!-- JQUERY -->
+<script
+    src="https://code.jquery.com/jquery-3.5.0.min.js"
+    integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ="
+    crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.payment/1.0.1/jquery.payment.min.js"></script>
+
+
 <script type="text/javascript" src="https://js.stripe.com/v2/"></script>
-
 <script type="text/javascript">
 
     $(function() {
 
-        var $form         = $(".require-validation");
+        var $form=$(".require-validation");
 
         $('form.require-validation').bind('submit', function(e) {
-            var $form         = $(".require-validation"),
+            var $form= $(".require-validation"),
                 inputSelector = ['input[type=email]', 'input[type=password]',
                     'input[type=text]', 'input[type=file]',
                     'textarea'].join(', '),
@@ -202,6 +257,8 @@
         }
 
     });
-</script>
 
-</html>
+</script>
+@endsection
+
+
