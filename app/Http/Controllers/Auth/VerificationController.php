898a81229a10c63fae4262943f724af3c47f3dd3
Bolobi <alban.jegoudez@live.fr>
2020-05-05 15:30:31 +0200

    Traduction de la vérification d'e-mail + ajout de cette fonctionalité dans la page parametres.blade.php.

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -26,7 +26,7 @@ class VerificationController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = "/";
+    protected $redirectTo = "/verif_email";
 
     /**
      * Create a new controller instance.
